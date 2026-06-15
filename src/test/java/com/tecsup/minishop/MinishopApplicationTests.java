3e26d3958c4a1057dd035cb063f3115708f7b3d9
Cesar Kana <cesar.kana@tecsup.edu.pe>
2026-06-15 20:44:02 +0100

    test: error deliberado para probar el pipeline

--- src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
@@ -1,13 +1,16 @@
-package com.tecsup.minishop;
+﻿package com.tecsup.minishop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class MinishopApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        fail("Error deliberado para probar el pipeline");
+    }
 
 }
