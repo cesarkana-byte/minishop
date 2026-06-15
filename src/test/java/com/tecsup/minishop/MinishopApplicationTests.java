976d0b6fe0d989074b4d70ed606134037e11ad67
Cesar Kana <cesar.kana@tecsup.edu.pe>
2026-06-15 21:37:26 +0100

    refactor: extraer constante en prueba de carga

--- src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class MinishopApplicationTests {
 
+    private static final String MENSAJE_PRUEBA = "La aplicacion carga correctamente";
+
     @Test
     void contextLoads() {
+        System.out.println(MENSAJE_PRUEBA);
     }
 
 }
