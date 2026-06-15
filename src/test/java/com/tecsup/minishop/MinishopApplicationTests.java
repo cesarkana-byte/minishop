9253f3a13b26338dfc4a90350b5fd829828fa224
Cesar Kana <cesar.kana@tecsup.edu.pe>
2026-06-15 20:52:53 +0100

    fix: restaurar prueba contextLoads

--- src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
+++ src/test/java/com/tecsup/minishop/MinishopApplicationTests.java
@@ -3,14 +3,11 @@ package com.tecsup.minishop;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 @SpringBootTest
 class MinishopApplicationTests {
 
     @Test
     void contextLoads() {
-        fail("Error deliberado para probar el pipeline");
     }
 
 }
